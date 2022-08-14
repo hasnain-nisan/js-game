02569dae63837718987609505d4a6f8510ae8ebc
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-08-14 18:53:25 +0600

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = canvas.width = 600
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = './shadow_dog.png'
+const spriteWidth = 575;
+const spriteHeight = 523;
+
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.fillRect(10, 20, 100, 100);
+    // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_HEIGHT, CANVAS_WIDTH)
+    requestAnimationFrame(animate);
+}
+
+animate()
+
+console.log(ctx);
