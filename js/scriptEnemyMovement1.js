73a574b8ed22d6d8c845d8c85c688f94944b9468
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-12-04 11:13:11 +0600

    Enemy movement pattern 1

---
+++ js/scriptEnemyMovement1.js
@@ -0,0 +1,68 @@
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 500);
+const CANVAS_HEIGHT = (canvas.height = 1000);
+const numberOfEnemies = 10;
+const enimies = [];
+let gameFrame = 0;
+
+// const enemyImage = new Image();
+// enemyImage.src = '../enemies/enemy1.png'
+
+class Enemy {
+    constructor() {
+        this.image = new Image();
+        this.image.src = "../enemies/enemy1.png";
+        this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (CANVAS_WIDTH - this.width);
+        this.y = Math.random() * (CANVAS_HEIGHT - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    }
+    update() {
+        this.x += (Math.random() * 5 - 2.5);
+        this.y += (Math.random() * 5 - 2.5);
+        if (gameFrame % this.flapSpeed === 0) {
+          this.frame > 4 ? (this.frame = 0) : this.frame++;
+        }
+    }
+    draw() {
+        ctx.drawImage(
+          this.image,
+          this.frame * this.spriteWidth,
+          0,
+          this.spriteWidth,
+          this.spriteHeight,
+          this.x,
+          this.y,
+          this.width,
+          this.height
+        );
+    }
+}
+
+for (let i = 0; i < numberOfEnemies; i++) {
+    let enemy = new Enemy();
+    enimies.push(enemy);
+}
+
+console.log(enimies);
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enimies.forEach((enemy) => {
+        enemy.update();
+        enemy.draw();
+    })
+    gameFrame++;
+    requestAnimationFrame(animate)
+}
+
+animate()
+
