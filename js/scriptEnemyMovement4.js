d5b3f9f3f2d57bf4b0b4adf96cd2670fabfc1b9b
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-12-04 13:40:42 +0600

    Enemy movement pattern 4

---
+++ js/scriptEnemyMovement4.js
@@ -0,0 +1,75 @@
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 500);
+const CANVAS_HEIGHT = (canvas.height = 1000);
+const numberOfEnemies = 100;
+const enimies = [];
+let gameFrame = 0;
+
+// const enemyImage = new Image();
+// enemyImage.src = '../enemies/enemy1.png'
+
+class Enemy {
+  constructor() {
+    this.image = new Image();
+    this.image.src = "../enemies/enemy4.png";
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 213;
+    this.spriteHeight = 213;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (CANVAS_WIDTH - this.width);
+    this.y = Math.random() * (CANVAS_HEIGHT - this.height);
+    this.newX = Math.random() * (CANVAS_WIDTH - this.width);
+    this.newY = Math.random() * (CANVAS_HEIGHT - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.interval = Math.floor(Math.random() * 200 + 50);
+  }
+  update() {
+    if(gameFrame % this.interval === 0){
+        this.newX = Math.random() * (CANVAS_WIDTH - this.width);
+        this.newY = Math.random() * (CANVAS_HEIGHT - this.height);
+    }
+    let dx = this.x - this.newX;
+    let dy = this.y - this.newY;
+    this.x -= dx/50;
+    this.y -= dy/50;
+    if (this.x + this.width < 0) this.x = CANVAS_WIDTH;
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+for (let i = 0; i < numberOfEnemies; i++) {
+  let enemy = new Enemy();
+  enimies.push(enemy);
+}
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enimies.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+
+animate();
